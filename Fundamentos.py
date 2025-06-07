06e28299a5a0fcaa40b8fd18bd95c46bdb8eb661
AlmaTrejo <altrejo.depad.cl@gmail.com>
2025-06-07 00:20:42 -0600

    Se agregan operadores
    se coloca un ejemplo de cada operados

---
+++ Fundamentos.py
@@ -0,0 +1,21 @@
+if __name__ == " __main__":
+
+    print("*****Operadores Aritmeticos*****")
+    print("Operador suma, 5+6=", 5 + 6 )    #
+    print("Operador resta, 9-8=", 9 - 8 )
+    print("Operador multiplicacion, 5*6=", 5 * 6 )  
+    print("Operador division, 5+6=", 20 / 6 )
+    print("Operador division entera, 30//4=", 30 //4)
+    print("Operador potencia, 3**6=", 3**6 )
+    print("Operador identidad, +6=", +6 )
+    print("Operador cambio de signo, 6=", -6 )
+    print("Operador modulo, 10%3=", 10%3 )
+    #comentario de una linea
+    '''comentario de 
+    varias lineas'''
+
+    print("*****Operadores Logicos*****")
+    print("True and True", True and True)
+#Hacer las tablas de verdad
+
+# Ejemplo de cada operador de comparación
